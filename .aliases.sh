fe4ef699564e71bf82297d00205a96bcf5e169e5
Brady Hawkins <brady93hawkins@gmail.com>
2024-02-10 21:58:57 +0100

    Add files via upload
    Organized dotfiles

---
+++ .aliases.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Stock Aliases                                                                               
+alias ll='ls -alF'                                                                                  
+alias la='ls -A'                                                                                     
+alias l='ls -CF'     
+
+
+#User defined Alias                                                                                  
+alias chrome='google-chrome-stable'                                                                  
+alias spot='spotify'                                                                                 
+alias apt-get='sudo apt-get'                                                                         
+alias voltron='cd /media/hawk0120/Voltron/'                                                         
+alias apt='sudo apt'                                                                                 
+alias nv='nvim'                                                                                     
+alias vim='nvim'                                                                                     
+alias ls='ls --color=auto -h'                                                                        
+alias home='cd ~/'                                                                                   
+alias rnh='cd ~/react-native-projects/'                                                              
+alias bashrc='nvim ~/.bashrc'                                                                        
+alias crna='npx create-react-native-app'                                                             
+alias exnl='expressvpn connect netherlands'                                                          
+alias exus='expressvpn connect united_states'                                                        
+alias exca='expressvpn connect canada'                                                               
+alias pmd='node /home/hawk0120/Tools/timer/alert.js'              
+alias visc='xdg-open ~/Pictures/vim.jpg/'                                                            
+
+#Git Alias
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gco='git checkout'	
+alias gb='git branch'
+alias gpl='git pull'
+alias gps='git push'
+alias gcl='git clone'
+alias glog='git log'
+alias gdiff='git diff'
+alias gmerge='git merge'
+alias greset='git reset'
+alias gtag='git tag'
+alias gf='git fetch'
+
+
