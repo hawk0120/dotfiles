6885c6a4377eec283d44c97f97da7a2ae9490811
Brady Hawkins <brady93hawkins@gmail.com>
2025-02-14 22:24:00 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 # Created: 2024-02-19
-# Updated: 2025-02-11
+# Updated: 2025-02-14
   
-############################################################################################################
-# This script installs the latest versions of the following programs:
-# 1. System Tools
-# 2. Programming Languages and Tools
-# 3. Communication and Entertainment
-############################################################################################################
+set -e  # Exit script on error
 
-set -e  # Exit on error
+echo "Updating system packages..."
+sudo apt update && sudo apt upgrade -y
 
 ############################################
 # System Tools
 ############################################
 
-# Update and upgrade
-sudo apt update && sudo apt upgrade -y
-
 # Install git
-if ! command -v git &> /dev/null; then
+if ! command -v git &>/dev/null; then
     sudo apt install -y git
     echo "Git installed"
 else
@@ -27,132 +20,122 @@ else
 fi
 
 # Install curl
-if ! command -v curl &> /dev/null; then
+if ! command -v curl &>/dev/null; then
     sudo apt install -y curl
     echo "Curl installed"
 else
     echo "Curl is already installed"
 fi
 
-# Install Neovim
-if ! command -v nvim &> /dev/null; then
-    sudo apt install -y neovim
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-    nvim +PackerSync +qall
-    echo "Neovim installed"
+# Install kitty
+if ! command -v kitty &>/dev/null; then
+    echo "Installing Kitty terminal..."
+    sudo apt install -y kitty
+    echo "Kitty installed"
 else
-    echo "Neovim is already installed"
-    nvim +PackerSync +qall
+    echo "Kitty is already installed"
 fi
 
 ############################################
 # Programming Languages and Tools
 ############################################
 
-# Remove default Java and install the latest OpenJDK
+# Remove default Java and install Java 17
+echo "Installing Java 17..."
 sudo apt remove -y default-jdk default-jre
-sudo apt install -y openjdk-21-jdk openjdk-21-jre
-java -version
-echo "Latest Java installed"
+sudo apt install -y openjdk-17-jdk openjdk-17-jre 
+echo "Java 17 installed"
 
-# Install the latest Node.js
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-sudo apt install -y nodejs
-node -v
-npm -v
-echo "Latest Node.js installed"
+# Install Node.js (latest LTS version)
+NODE_LTS="20"
+INSTALLED_NODE=$(node -v 2>/dev/null | cut -d "v" -f 2 | cut -d "." -f 1)
 
-# Install IntelliJ Community Edition
-if ! command -v intellij-idea-community &> /dev/null; then
-    wget -O intellij.tar.gz https://download.jetbrains.com/product?code=IIC&latest&distribution=linux
-    sudo tar -xvf intellij.tar.gz -C /opt/
-    rm intellij.tar.gz
-    echo "IntelliJ installed"
+if [ "$INSTALLED_NODE" != "$NODE_LTS" ]; then
+    echo "Installing Node.js $NODE_LTS..."
+    curl -fsSL https://deb.nodesource.com/setup_$NODE_LTS.x | sudo -E bash -
+    sudo apt install -y nodejs
+    echo "Node.js $NODE_LTS installed"
 else
-    echo "IntelliJ is already installed"
+    echo "Node.js is already up to date"
 fi
 
 ############################################
 # Communication and Entertainment
 ############################################
 
-# Install Microsoft Teams
-if ! command -v teams &> /dev/null; then
-    wget https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.4.00.13653_amd64.deb
-    sudo dpkg -i teams_1.4.00.13653_amd64.deb || sudo apt install -f -y
-    rm teams_1.4.00.13653_amd64.deb
-    echo "Microsoft Teams installed"
-else
-    echo "Microsoft Teams is already installed"
-fi
-
-# Install Zoom
-if ! command -v zoom &> /dev/null; then
-    wget https://zoom.us/client/latest/zoom_amd64.deb
-    sudo dpkg -i zoom_amd64.deb || sudo apt install -f -y
-    rm zoom_amd64.deb
-    echo "Zoom installed"
-else
-    echo "Zoom is already installed"
-fi
-
-# Install Spotify
-if ! command -v spotify &> /dev/null; then
-    curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
-    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-    sudo apt update && sudo apt install -y spotify-client
-    echo "Spotify installed"
-else
-    echo "Spotify is already installed"
-fi
-
-# Install Blueman
-if ! command -v blueman-manager &> /dev/null; then
-    sudo apt install -y blueman
-    echo "Blueman installed"
-else
-    echo "Blueman is already installed"
-fi
-
 # Install VLC
-if ! command -v vlc &> /dev/null; then
+if ! command -v vlc &>/dev/null; then
+    echo "Installing VLC..."
     sudo apt install -y vlc
     echo "VLC installed"
 else
     echo "VLC is already installed"
 fi
 
-# Install Adobe Acrobat Reader
-if ! command -v acrordrdc &> /dev/null; then
-    sudo snap install acrordrdc
-    echo "Adobe Acrobat Reader installed"
+# Install Blueman
+if ! command -v blueman-manager &>/dev/null; then
+    echo "Installing Blueman..."
+    sudo apt install -y blueman
+    echo "Blueman installed"
 else
-    echo "Adobe Acrobat Reader is already installed"
+    echo "Blueman is already installed"
 fi
+
+############################################
+# Rofi + Themes
+############################################
+
+if ! command -v rofi &>/dev/null; then
+    echo "Installing Rofi..."
+    sudo apt install -y rofi
+    echo "Rofi installed"
+else
+    echo "Rofi is already installed"
+fi
+
+# Install Rofi Tokyonight Themes
+echo "Installing Rofi Tokyonight themes..."
+mv ~/.config/rofi ~/.config/rofi.bak || true
+git clone git@github.com:w8ste/Tokyonight-rofi-theme.git ~/.config/rofi
+sudo mv ~/.config/rofi/tokyonight.rasi /usr/share/rofi/themes
+sudo mv ~/.config/rofi/tokyonight_big1.rasi /usr/share/rofi/themes
+sudo mv ~/.config/rofi/tokyonight_big2.rasi /usr/share/rofi/themes
+rm ~/.config/rofi/README.md
+echo "Rofi Tokyonight themes installed"
+
+############################################
+# Install Calibre
+############################################
+
+echo "Installing Calibre..."
+wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+echo "Calibre installed"
 
 ############################################
 # Clone and move dotfiles repository
 ############################################
 
-if [ ! -d ~/dotfiles ]; then
-    git clone git@github.com:hawk0120/dotfiles.git ~/dotfiles
+DOTFILES_DIR="$HOME/dotfiles"
+
+if [ ! -d "$DOTFILES_DIR" ]; then
+    echo "Cloning dotfiles repository..."
+    git clone git@github.com:hawk0120/dotfiles.git "$DOTFILES_DIR"
     echo "Dotfiles repository cloned"
 else
     echo "Dotfiles repository already exists"
 fi
 
-if [ -d ~/dotfiles ]; then
-    cp -r ~/dotfiles/.bashrc ~/
-    cp -r ~/dotfiles/.aliases.sh ~/
-    cp -r ~/dotfiles/.gitconfig ~/
-    cp -r ~/dotfiles/nvim ~/
+if [ -d "$DOTFILES_DIR" ]; then
+    echo "Moving dotfiles..."
+    cp -r "$DOTFILES_DIR/.bashrc" "$HOME/"
+    cp -r "$DOTFILES_DIR/.aliases.sh" "$HOME/"
+    cp -r "$DOTFILES_DIR/.gitconfig" "$HOME/"
+    cp -r "$DOTFILES_DIR/nvim" "$HOME/.config/"
     echo "Dotfiles moved to home directory"
 else
     echo "Failed to clone the repository"
 fi
 
-# Clean up
-rm -rf ~/dotfiles
+rm -rf "$DOTFILES_DIR"
 
-echo "Script completed successfully"
+echo "Script completed successfully!"
